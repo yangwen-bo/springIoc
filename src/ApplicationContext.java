0e528d59de0102113f910d7ec33f29511a596592
yangwenbo <yangwenbo@qq.com>
2019-08-13 10:07:59 +0800

    提交

---
+++ src/ApplicationContext.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lenovo on 2019/8/12.
+ * @ yangwen-bo
+ *
+ * 读取 XML 文件，获取 POJO 的相关信息
+ * 定义 getBean 方法，通过 id 返回 bean
+ */
+public interface ApplicationContext {
+    public Object getBean(String id);
+}
